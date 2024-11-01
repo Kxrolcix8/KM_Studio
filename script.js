9d705b5d573e07cb594fb57289b8b8fb58d9aadc
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2024-11-01 15:56:41 +0100

    dodanie plików

---
+++ script.js
@@ -0,0 +1,12 @@
+function calculatePrice() {
+    const pricePerPhoto = 20;
+    let totalPhotos = 0;
+
+    // Sprawdź, czy checkboxy są zaznaczone
+    if (document.getElementById('zgoda1').checked) totalPhotos++;
+    if (document.getElementById('zgoda2').checked) totalPhotos++;
+
+    // Oblicz cenę i wyświetl ją w labelu
+    const totalPrice = totalPhotos * pricePerPhoto;
+    document.getElementById('price-label').textContent = `Cena: ${totalPrice} zł`;
+}
