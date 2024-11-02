deaaff445779d88e420285d7d774a904abc57e2f
Karolina <131374467+Kxrolcix8@users.noreply.github.com>
2024-11-02 01:09:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ function calculatePrice() {
     // Sprawdź, czy checkboxy są zaznaczone
     if (document.getElementById('zgoda1').checked) totalPhotos++;
     if (document.getElementById('zgoda2').checked) totalPhotos++;
-
+    if (document.getElementById('zgoda3').checked) totalPhotos++;
+    
     // Oblicz cenę i wyświetl ją w labelu
     const totalPrice = totalPhotos * pricePerPhoto;
     document.getElementById('price-label').textContent = `Cena: ${totalPrice} zł`;
